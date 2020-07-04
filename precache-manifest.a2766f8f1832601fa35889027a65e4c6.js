d7a43fd1025c9657fbdad6833a38487df5fbb782
tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2020-07-04 19:56:34 +0530

    Updates

---
+++ precache-manifest.a2766f8f1832601fa35889027a65e4c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffef86c50d0592b5dc9c7dd931b8cf4b",
+    "url": "/CVMaker/index.html"
+  },
+  {
+    "revision": "e3b6a7c3429029ce0a21",
+    "url": "/CVMaker/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "e3b6a7c3429029ce0a21",
+    "url": "/CVMaker/static/js/2.c8413ca8.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/CVMaker/static/js/2.c8413ca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0eb44c315f3ff47039",
+    "url": "/CVMaker/static/js/main.9b095a4e.chunk.js"
+  },
+  {
+    "revision": "8e2f22f7e6b20d4e6a6b",
+    "url": "/CVMaker/static/js/runtime-main.ad4e5cea.js"
+  }
+]);
